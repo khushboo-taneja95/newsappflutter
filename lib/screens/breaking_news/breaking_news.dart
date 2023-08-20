10307e4130b612f15dc15074d11c7d60f2a6f5e2
Shreeram <khushbutaneja95@gmail.com>
2023-08-20 11:17:01 +0530

    ui design

--- lib/screens/breaking_news/breaking_news.dart
+++ lib/screens/breaking_news/breaking_news.dart
@@ -19,10 +19,7 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const ToolbarWithTitleAndBackbutton(),
-            listData()
-          ],
+          children: [const ToolbarWithTitleAndBackbutton(), listData()],
         ),
       ),
     );
@@ -54,12 +51,14 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10.0),
                 image: DecorationImage(
-                    image: AssetImage('assets/pakhead.png'),
-                    fit: BoxFit.fill)),
+                    image: AssetImage('assets/pakhead.png'), fit: BoxFit.fill)),
+          ),
+          SizedBox(
+            height: 2,
           ),
           Text(
-            'Damage of Geroues',
-            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+            'Express Investigations',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
           ),
         ],
       ),
@@ -78,8 +77,8 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Container(
-                  width: 40,
-                  height: 40,
+                  width: 30,
+                  height: 30,
                   decoration: BoxDecoration(
                     border: Border.all(width: 2),
                     shape: BoxShape.circle,
@@ -91,7 +90,7 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
                       style: const TextStyle(
                           color: Colors.white,
                           fontWeight: FontWeight.bold,
-                          fontSize: 10),
+                          fontSize: 8),
                     ),
                   ),
                 ),
@@ -101,7 +100,10 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
                 Text(
                   "CNN India",
                   style: const TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12,
+                  ),
                 ),
                 const SizedBox(
                   width: 10,
@@ -109,7 +111,10 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
                 Text(
                   "-",
                   style: const TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
                 ),
                 const SizedBox(
                   width: 5,
@@ -117,7 +122,10 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
                 Text(
                   "Feb 28, 2023",
                   style: const TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12,
+                  ),
                 ),
                 const SizedBox(
                   width: 5,
@@ -125,7 +133,10 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
                 Text(
                   "-",
                   style: const TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
                 ),
                 const SizedBox(
                   width: 5,
@@ -133,7 +144,10 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
                 Text(
                   "World",
                   style: const TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12,
+                  ),
                 ),
               ],
             ),
@@ -142,11 +156,11 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
           Padding(
             padding: const EdgeInsets.all(4.0),
             child: Text(
-              "Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite Sky Perfect JSAT order",
+              "Drug pandemic in J&K: Networks of terror intersect with drug supply lines, on ground, from sky and online",
               style: const TextStyle(
                 color: PanthalassaColors.colorBlack,
                 fontWeight: FontWeight.bold,
-                fontSize: 14,
+                fontSize: 16,
               ),
             ),
           ),
@@ -155,26 +169,42 @@ class _BreakingNewsDetailsState extends State<BreakingNewsDetails> {
           const SizedBox(height: 15.0),
           Padding(
             padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+            child: Text(
+              'From the sky, drones from Pakistan that drop drugs across the border; on the ground, a distribution model akin to a multi-level marketing scheme where an addict ropes in more addicts; consignments ferried from neighbouring states; prescription drugs sold against cancelled licences; and opioids procured from the darknet.',
+              style: const TextStyle(
+                fontSize: 12,
+                height: 1.5,
+                color: Colors.black,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+            child: Text(
+              'These are the myriad supply lines that feed and fuel the drug pandemic in Jammu and Kashmir which has stretched the public health system to its limits and poses a set of unprecedented challenges to security agencies, an investigation by The Indian Express has found. Analysing police records of seizure and arrests, both at an all-time high, and interviews with top officials also reveals that, in many cases, the drug trade and terror networks also run parallel — and often intersect.',
+              style: const TextStyle(
+                fontSize: 12,
+                height: 1.5,
+                color: Colors.black,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0, right: 4.0),
             child: ReadMoreText(
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite\n'
-              'Sky Perfect JSAT order first satelite Sky Perfect JSAT order first Airbus satelite',
+              'From the sky, drones from Pakistan that drop drugs across the border; on the ground, a distribution model akin to a multi-level marketing scheme where an addict ropes in more addicts; consignments ferried from neighbouring states; prescription drugs sold against cancelled licences; and opioids procured from the darknet.\n'
+              'These are the myriad supply lines that feed and fuel the drug pandemic in Jammu and Kashmir which has stretched the public health system to its limits and poses a set of unprecedented challenges to security agencies, an investigation by The Indian Express has found. Analysing police records of seizure and arrests, both at an all-time high, and interviews with top officials also reveals that, in many cases, the drug trade and terror networks also run parallel — and often intersect.\n'
+              'A recent, illustrative example is from June 9 when, trying to flee Rangoor village near the international border, three alleged narcotic smugglers from Punjab were intercepted and caught by the Jammu & Kashmir police. In their alleged possession was heroin weighing 2.8 kg, a pistol, Rs 93,000, and four high-end phones.',
               numLines: 15,
               readMoreText: 'Read more',
               readLessText: 'Read less',
               style: const TextStyle(
-                fontSize: 14,
+                fontSize: 12,
+                height: 1.5,
                 color: Colors.black,
                 fontWeight: FontWeight.w300,
               ),
