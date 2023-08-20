c33455cae53104328312639f4f12914d210652b1
kumargauraw989@gmail.com <Kum@rgrv9>
2023-08-20 06:59:27 -0700

    navigation

--- lib/screens/home/home_navigations.dart
+++ lib/screens/home/home_navigations.dart
@@ -133,8 +133,97 @@ class _HomeNavigationState extends State<HomeNavigation> {
         ],
         backgroundColor: PanthalassaColors.colorWhite,
       ),
-      drawer: Drawer(
-        child: getDrawer(),
+      drawer: Container(
+        width: MediaQuery.of(context).size.width * 0.7,
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: PanthalassaColors.textColorWhite,
+            ),
+            borderRadius: const BorderRadius.only(topRight:Radius.circular(10),bottomRight:Radius.circular(10))
+        ),
+        child: Drawer(
+          backgroundColor: PanthalassaColors.textColorWhite,
+          child: ListView(
+            // Remove padding
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('Customer'),
+                accountEmail: Text('customer@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  child: ClipOval(
+                    child: Image.network(
+                      'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
+                      fit: BoxFit.cover,
+                      width: 90,
+                      height: 90,
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                          'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg')),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.favorite),
+                title: Text('Favorites'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Friends'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.share),
+                title: Text('Share'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text('Request'),
+                onTap: () => null,
+                trailing: ClipOval(
+                  child: Container(
+                    color: Colors.red,
+                    width: 20,
+                    height: 20,
+                    child: Center(
+                      child: Text(
+                        '8',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.description),
+                title: Text('Policies'),
+                onTap: () => null,
+              ),
+              Divider(),
+              ListTile(
+                title: Text('Exit'),
+                leading: Icon(Icons.exit_to_app),
+                onTap: () => null,
+              ),
+            ],
+          ),
+        ),
       ),
       backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBar(
@@ -215,136 +304,219 @@ class _HomeNavigationState extends State<HomeNavigation> {
 
   Widget getDrawer() {
     return SafeArea(
-      child: SizedBox(
-        width: 60,
-        child: Drawer(
-          child: Column(children: [
-            Expanded(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  const DrawerHeader(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          "Panthalassa",
-                          style: TextStyle(
-                              fontSize: 22.0,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Version: 1.0.3",
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
+      child: /*Drawer(
+        child: Column(children: [
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                const DrawerHeader(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Panthalassa",
+                        style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Version: 1.0.3",
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black),
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                    child: SettingItem(
-                      title: "Bookmarks",
-                      leadingIcon: "assets/ic_bookmarks.png",
-                      onTap: () {},
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: SettingItem(
+                    title: "Bookmarks",
+                    leadingIcon: "assets/ic_bookmarks.png",
+                    onTap: () {},
                   ),
-                  Divider(
-                    height: 0,
-                    color: Colors.grey.withOpacity(0.8),
+                ),
+                Divider(
+                  height: 0,
+                  color: Colors.grey.withOpacity(0.8),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: SettingItem(
+                    title: "Get Notifications",
+                    leadingIcon: "assets/ic_notifications.png",
+                    onTap: () {},
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                    child: SettingItem(
-                      title: "Get Notifications",
-                      leadingIcon: "assets/ic_notifications.png",
-                      onTap: () {},
-                    ),
+                ),
+                Divider(
+                  height: 0,
+                  color: Colors.grey.withOpacity(0.8),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: SettingItem(
+                    title: "Privacy Policy",
+                    leadingIcon: "assets/ic_privacypolicy.png",
+                    onTap: () {},
                   ),
-                  Divider(
-                    height: 0,
-                    color: Colors.grey.withOpacity(0.8),
+                ),
+                Divider(
+                  height: 0,
+                  color: Colors.grey.withOpacity(0.8),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: SettingItem(
+                    title: "Rate This App",
+                    leadingIcon: "assets/ic_rate_the_app.png",
+                    onTap: () {},
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                    child: SettingItem(
-                      title: "Privacy Policy",
-                      leadingIcon: "assets/ic_privacypolicy.png",
-                      onTap: () {},
-                    ),
-                  ),
-                  Divider(
-                    height: 0,
-                    color: Colors.grey.withOpacity(0.8),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                    child: SettingItem(
-                      title: "Rate This App",
-                      leadingIcon: "assets/ic_rate_the_app.png",
-                      onTap: () {},
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (ctx) => const WelcomeScreen()));
-              },
-              child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 50.0, left: 10.0),
-                    child: Column(
-                      children: [
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                height: 40,
-                                width: 40,
-                                decoration: BoxDecoration(
-                                  boxShadow: const [
-                                    BoxShadow(
-                                        color: Colors.black54,
-                                        blurRadius: 5.0,
-                                        offset: Offset(0.0, 0.75))
-                                  ],
-                                  color: PanthalassaColors.colorRed,
-                                  borderRadius: BorderRadius.circular(50 / 2),
-                                ),
-                                child: Center(
-                                  child: Image.asset(
-                                    'assets/ic_logout.png',
-                                    width: 22,
-                                    height: 22,
-                                    color: Colors.white,
-                                  ),
-                                ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (ctx) => const WelcomeScreen()));
+            },
+            child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0, left: 10.0),
+                  child: Column(
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                boxShadow: const [
+                                  BoxShadow(
+                                      color: Colors.black54,
+                                      blurRadius: 5.0,
+                                      offset: Offset(0.0, 0.75))
+                                ],
+                                color: PanthalassaColors.colorRed,
+                                borderRadius: BorderRadius.circular(50 / 2),
                               ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              const Expanded(
-                                child: Text(
-                                  'Logout',
-                                  style: TextStyle(fontSize: 16),
+                              child: Center(
+                                child: Image.asset(
+                                  'assets/ic_logout.png',
+                                  width: 22,
+                                  height: 22,
+                                  color: Colors.white,
                                 ),
                               ),
-                            ]),
-                      ],
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            const Expanded(
+                              child: Text(
+                                'Logout',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          ]),
+                    ],
+                  ),
+                )),
+          )
+        ]),
+      ),*/
+      Container(
+        width: MediaQuery.of(context).size.width * 0.5,
+        child: Drawer(
+          width: 50,
+          child: ListView(
+            // Remove padding
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('Customer'),
+                accountEmail: Text('customer@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  child: ClipOval(
+                    child: Image.network(
+                      'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
+                      fit: BoxFit.cover,
+                      width: 90,
+                      height: 90,
                     ),
-                  )),
-            )
-          ]),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                          'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg')),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.favorite),
+                title: Text('Favorites'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Friends'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.share),
+                title: Text('Share'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text('Request'),
+                onTap: () => null,
+                trailing: ClipOval(
+                  child: Container(
+                    color: Colors.red,
+                    width: 20,
+                    height: 20,
+                    child: Center(
+                      child: Text(
+                        '8',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.description),
+                title: Text('Policies'),
+                onTap: () => null,
+              ),
+              Divider(),
+              ListTile(
+                title: Text('Exit'),
+                leading: Icon(Icons.exit_to_app),
+                onTap: () => null,
+              ),
+            ],
+          ),
         ),
       ),
     );
